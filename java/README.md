7c4407d40185fc910af3672ca556086bfb842201
Dongri Jin <dongrify@gmail.com>
2019-03-18 22:27:56 +0900

    auto commit

---
+++ java/README.md
@@ -0,0 +1,31 @@
+# gradle install
+
+```
+$ brew install gradle
+```
+
+# eclipse install
+
+```
+$ brew cask install eclipse-java
+```
+
+# gradle eclipse
+
+```
+$ gradle eclipse
+```
+
+# eclipse
+import this project
+
+# war
+```
+$ gradle war
+```
+
+# java version
+```
+$ javac -version
+javac 10.0.2
+```
---
+++ java/src/main/java/com/github/dongri/docker/jetty/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.github.dongri.docker.jetty;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("Hello World!");
+    }
+}
